01d9fa0ed28f40aa5f89b89b82e8afd1886321a5
Thorsten Geßler <thorsten.gessler@proton.me>
2026-03-03 12:07:24 +0100

    um function erweitert

--- einstiegFunctions.py
+++ einstiegFunctions.py
@@ -1,1 +1,20 @@
+
+#### Bereich für die Functions
+
+def getSumme(a, b):
+    return a + b
+
+
+#### Hier beginnt mein Hauptprogramm
+
 print("Einstiegsbeispiel zum Thema Functions in Python")
+
+x = 7
+y = 8
+
+
+print(getSumme(x,y))
+
+
+
+
